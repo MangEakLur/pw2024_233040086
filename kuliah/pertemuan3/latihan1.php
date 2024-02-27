7be80e0522c214d0a617f32a6eb8a22ecbcefa47
MangEakLur <Iqbalnurfikri20@gmail.com>
2024-02-27 15:32:47 +0700

    memperbaiki struktur folder

---
+++ kuliah/pertemuan3/latihan1.php
@@ -0,0 +1,16 @@
+<?php
+echo "Hello Wolrd!";
+echo "<br>";
+echo "Kuliah Pemograman Web";
+echo "<br>";
+echo "123 + 10";
+echo "<br>";
+echo 3.14;
+echo "<br>";
+echo false;
+echo "<br>";
+echo "Jum'at";
+echo "<br>";
+echo '"Iqbal" kuliah hari ' ."Jum'at";
+echo "<br>";
+echo "\"Iqbal\" kuliah hari Jum'at";
